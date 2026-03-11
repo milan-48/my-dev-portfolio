2d6a97a323bfa05d0ef3b5dcd501cbf39c3d4a6d
MilanWhyphy <milan.whyphy@gmail.com>
2026-03-11 00:40:27 -0400

    Remove Lenis library and smooth scrolling components; update global styles for smoother scrolling behavior

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SmoothScrollWrapper from "../components/smooth-scroll-wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,7 +38,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SmoothScrollWrapper />
         {children}
       </body>
     </html>
--- components/smooth-scroll-wrapper.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const SmoothScroll = dynamic(() => import("./smooth-scroll"), { ssr: false });
-
-export default function SmoothScrollWrapper() {
-  return <SmoothScroll />;
-}
